83faf7044c98406e35b32bc0540efd9de1e19b0b
massi.brero@gmail.com <massi.brero@gmail.com>
2020-05-21 10:10:27 +0200

    changed folders

--- pwa-mb/service-worker.js
+++ pwa-mb/service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/images/logo.png'])
+      return cache.addAll(['/', '/index.html', '/images/*'])
     })
   )
 })
