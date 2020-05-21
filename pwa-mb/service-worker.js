82176cef03e3bcc29e154ffb56fd8813b5f09851
massi.brero@gmail.com <massi.brero@gmail.com>
2020-05-21 10:18:06 +0200

    commit

--- pwa-mb/service-worker.js
+++ pwa-mb/service-worker.js
@@ -1,14 +1,20 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/images/logo.png'])
+var cacheName = 'pwa-mb'
+var filesToCache = ['/', '/index.html', '/css/styles.css', '/js/app.js']
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache)
     })
   )
 })
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request)
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request)
     })
   )
 })
