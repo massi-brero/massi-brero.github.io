3f9df4264a4d64ed50575e33153d3eecf1c4422c
massi.brero@gmail.com <massi.brero@gmail.com>
2020-05-21 13:12:42 +0200

    commit

--- pwa-mb/app.js
+++ pwa-mb/app.js
@@ -2,4 +2,9 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('./service-worker.js')
   })
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      registration.update()
+    }
+  })
 }
