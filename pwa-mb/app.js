216b218929a5ef793ab3d2cdb6a3c2398cd220d3
Brero, Massimiliano <massimiliano@adess.de>
2020-05-22 11:00:16 +0200

    workbox integration

--- pwa-mb/app.js
+++ pwa-mb/app.js
@@ -1,10 +1,5 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-  })
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    for (let registration of registrations) {
-      registration.update()
-    }
-  })
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js");
+  });
 }
--- pwa-mb/service-worker.js
+++ pwa-mb/service-worker.js
@@ -1,20 +1,35 @@
-var cacheName = 'pwa-mb'
-var filesToCache = ['./', './index.html', './css/styles.css', './app.js']
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+let cacheName = "pwa-mb";
+let filesToCache = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./app.js",
+  "/charts/pie_chart.jpg",
+];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache)
+      return cache.addAll(filesToCache);
     })
-  )
-})
+  );
+});
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
-      return response || fetch(e.request)
+      return response || fetch(e.request);
     })
-  )
-})
+  );
+});
+
+workbox.routing.registerRoute(
+    /\.(?:html|js|css)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'static-resources'
+    })
+);
