8adfe3fcbd359802d20cea162d2283ed08d05922
Brero, Massimiliano <massimiliano@adess.de>
2020-05-23 16:54:18 +0200

    commit

--- pwa-mb/app.js
+++ pwa-mb/app.js
@@ -1,5 +1,21 @@
-if ("serviceWorker" in navigator) {
+const check = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker Support!");
+  }
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support!");
+  }
+};
+
+const registerServiceWorker = () => {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("./service-worker.js");
   });
-}
+};
+
+const main = () => {
+  check();
+  registerServiceWorker();
+};
+
+main();
