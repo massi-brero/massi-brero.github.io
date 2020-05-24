020291f8ec54f7f645a15c0990a5f53ec8e07756
Brero, Massimiliano <massimiliano@adess.de>
2020-05-24 12:43:23 +0200

    commit

---
+++ pwa-mb-push/app.js
@@ -0,0 +1,51 @@
+const check = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker Support!");
+  }
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support!");
+  }
+};
+
+const registerServiceWorker = async () => {
+  return await navigator.serviceWorker.register("./service-worker.js");
+};
+
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission();
+  if (permission !== "granted") {
+    throw new Error("Permission not granted for Notification");
+  }
+
+  return permission;
+};
+
+const showLocalNotification = (title, body, swRegistration) => {
+  const options = {
+    body,
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    tag: "vibration-sample",
+  };
+
+  navigator.serviceWorker.ready.then(registration => {
+    registration.showNotification(title, options).then(NotificationEvent => {
+      console.log(NotificationEvent);
+    })
+  });
+};
+
+const main = async () => {
+  check();
+  const swRegistration = await registerServiceWorker();
+  const permission = await requestNotificationPermission();
+  if (permission === "granted") {
+    showLocalNotification(
+      "Neue Statistiken verfügbar",
+      "Bitte aktualisieren Sie die IMVS-Stats App"
+    );
+  }
+};
+
+// main().then(() => {
+//   console.log("application started");
+// });
---
+++ pwa-mb-push/service-worker.js
@@ -0,0 +1,56 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+let cacheName = "pwa-mb";
+let filesToCache = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./app.js",
+  "./charts/pie_chart.jpg",
+  "./charts/columns_chart.png",
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", async () => {
+  try {
+    console.log("activate");
+    const options = {};
+    const subscription = await self.registration.pushManager.subscribe(options);
+    console.log(subscription);
+  } catch (e) {
+    console.log("Error", e);
+  }
+});
+
+/* Serve cached content when offline */
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+workbox.routing.registerRoute(
+  /\.(?:html|js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "static-resources",
+  })
+);
+
+workbox.routing.registerRoute(
+  /\/charts\/.*(?:png|jpg|svg)$/,
+  new workbox.strategies.NetworkFirst({
+    cacheName: "static-resources",
+  })
+);
